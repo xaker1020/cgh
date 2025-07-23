0bb3cc0b52c96ffe3d2e06f76874b979953ca7b7
xaker1020 <qodirjonovhabibulloh8@gmail.com>
2025-07-23 16:03:04 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+  var typed = new Typed('#h1', {
+      strings: ["qodirjonov habibulloh"],
+      typeSpeed: 100,
+      loop:true
+    });
+      var typed2 = new Typed('#p', {
+      strings: ["Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsum, neque."],
+      typeSpeed: 100,
+      loop:true
+    });
+    
